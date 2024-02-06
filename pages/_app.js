4fefd902d78bae6fcc8fc5f533419e53c89a6416
Harsh Paliwal <harshbca.21@stu.srmuniversity.ac.in>
2024-02-06 21:16:31 +0530

    Basic Redux toolkit setup has been done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import { Provider } from 'react-redux';
+import store from '@/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
+
+
 import "@/styles/globals.scss";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  )
 }
+
+export default App;
