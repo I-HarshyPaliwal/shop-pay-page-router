c18eb94605ab5fff44d4da8abd7c138104285d2a
Harsh Paliwal <harshbca.21@stu.srmuniversity.ac.in>
2024-02-06 19:05:31 +0530

    Created db.js with connectDb() and disConnectDb()

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import db from "@/utils/db";
+
 export default function handler(req, res) {
+  db.connectDb();
+  db.disConnectDb();
   res.status(200).json({ name: "John Doe" });
 }
