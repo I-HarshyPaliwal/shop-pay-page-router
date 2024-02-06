8a99dd1aa1eea445c8a87bfac89448826df3760b
Harsh Paliwal <harshbca.21@stu.srmuniversity.ac.in>
2024-02-06 21:48:18 +0530

    Made CSS Global Variables accessible via next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+const __dirname = path.resolve();
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname , "styles")],
+    prependData : `@import "base.scss";`,
+  },
 };
 
 export default nextConfig;
