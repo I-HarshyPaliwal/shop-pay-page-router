a71d95b8a25299354920831c84bdc07e7f54aaad
Harsh Paliwal <harshbca.21@stu.srmuniversity.ac.in>
2024-02-09 01:08:02 +0530

    Constructed Top Part of the header | Section-3 | 2

--- components/header/Top.js
+++ components/header/Top.js
@@ -1,7 +1,54 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
 import styles from './styles.module.scss';
+import { SiSpringsecurity } from "react-icons/si";
+import { CiHeart } from "react-icons/ci";
+import { RiCustomerService2Line } from "react-icons/ri";
+import { RiAccountPinCircleLine , RiArrowDropDownFill } from "react-icons/ri";
+import { IoHelp } from "react-icons/io5";
+import Link from 'next/link';
+
 
 export default function Top() {
   return (
-    <div>Top</div>
+    <div className={styles.top}>
+        <div className={styles.top__container}>
+            <div>
+
+            </div>
+            <ul className={styles.top__list}>
+                <li>
+                    <img  src="https://flagcdn.com/in.svg" alt="Indian Flag" />
+                    <span>India</span>
+                </li>
+                <li>
+                <SiSpringsecurity />
+                   <span>Buyer Protection </span>
+                </li>
+                <li>
+                <RiCustomerService2Line />
+                    <span>Customer Service</span>
+                </li>
+                <li>
+                <IoHelp />
+                    <span>Help</span>
+                </li>
+                <li>
+                <CiHeart />
+                <Link href='/profile/wishlist'>
+                    <span>Wishlist</span>
+                </Link>
+                </li>
+                <li>
+                    <div className={styles.flex}>
+                    <RiAccountPinCircleLine />
+                    <span>Account</span>
+                    <RiArrowDropDownFill />
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+    </div>
   )
 }
