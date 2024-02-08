24a57354dcc3899bc530f23eec3ef30a47bcbc95
Harsh Paliwal <harshbca.21@stu.srmuniversity.ac.in>
2024-02-08 23:50:44 +0530

    Added assets to public folder

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div className={styles.red}> 
-      <h1>Welcome</h1></div>
+      <Header />
     </>
   );
 }
