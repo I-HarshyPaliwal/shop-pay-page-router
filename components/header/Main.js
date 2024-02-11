574ff9db93788b16ded6c0c5b535f6aa3f337500
Harsh Paliwal <harshbca.21@stu.srmuniversity.ac.in>
2024-02-11 13:48:14 +0530

    Header Component work is complete

---
+++ components/header/Main.js
@@ -0,0 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import styles from './styles.module.scss';
+import { RiSearchLine } from "react-icons/ri";import { FaOpencart } from "react-icons/fa";
+import { useSelector } from 'react-redux';
+
+
+
+export default function Main() {
+    const {cart} = useSelector((state)=>( {...state}));
+  
+    return (
+    
+    <div className={styles.main}>
+        <div className={styles.main__container}>
+            <Link href='/'>
+                <div className={styles.logo}>
+                    <img src="../../../logo.png" alt="" /> 
+                    {/* '@/address' doesn't work with this */}
+                </div>
+            </Link>
+            <div className={styles.search}>
+                <input type="text" name="" id="" placeholder='Search...'/>
+                <div className={styles.search__icon}><RiSearchLine /></div>
+            </div>
+            <Link href='/'>
+                <div className={styles.cart}>
+                    <FaOpencart />
+                    <span>{cart.length}</span>
+                </div>
+            </Link>
+
+        </div>
+    </div>
+  )
+}
--- components/header/Top.js
+++ components/header/Top.js
@@ -7,9 +7,14 @@ import { RiCustomerService2Line } from "react-icons/ri";
 import { RiAccountPinCircleLine , RiArrowDropDownFill } from "react-icons/ri";
 import { IoHelp } from "react-icons/io5";
 import Link from 'next/link';
+import { useState } from 'react';
+import UserMenu from './UserMenu';
 
 
 export default function Top() {
+
+    const [loggedIn , setLoggedIn] = useState(true);
+    const [visible , setVisible] = useState(false);
   return (
     <div className={styles.top}>
         <div className={styles.top__container}>
@@ -17,34 +22,46 @@ export default function Top() {
 
             </div>
             <ul className={styles.top__list}>
-                <li>
+                <li className={styles.li}>
                     <img  src="https://flagcdn.com/in.svg" alt="Indian Flag" />
                     <span>India</span>
                 </li>
-                <li>
+                <li className={styles.li}>
                 <SiSpringsecurity />
                    <span>Buyer Protection </span>
                 </li>
-                <li>
+                <li className={styles.li}>
                 <RiCustomerService2Line />
                     <span>Customer Service</span>
                 </li>
-                <li>
+                <li className={styles.li}>
                 <IoHelp />
                     <span>Help</span>
                 </li>
-                <li>
+                <li className={styles.li}>
                 <CiHeart />
                 <Link href='/profile/wishlist'>
                     <span>Wishlist</span>
                 </Link>
                 </li>
-                <li>
+                <li className={styles.li} 
+                onMouseOver={()=>setVisible(true)}
+                onMouseLeave={()=>setVisible(false)}
+                >
+                {loggedIn? (<li className={styles.li}>
+                    <div className={styles.flex}>
+                    <img src="https://cdn.dribbble.com/users/6104671/screenshots/14274019/media/2acc50ba9f18dcb497ca845314007549.jpg?resize=400x300&vertical=center" alt="" />
+                    <span>Harsh Paliwal</span>
+                    <RiArrowDropDownFill />
+                    </div>
+                </li>):(<li className={styles.li}>
                     <div className={styles.flex}>
                     <RiAccountPinCircleLine />
                     <span>Account</span>
                     <RiArrowDropDownFill />
                     </div>
+                </li>) }
+                {visible && <UserMenu loggedIn={loggedIn}/>}
                 </li>
 
             </ul>
