41feee45e3222d0035cb5415165d534fd6bc2ff8
Harsh Paliwal <harshbca.21@stu.srmuniversity.ac.in>
2024-02-19 10:36:04 +0530

    I tried to create email functionality while watching project video but it didn't work

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,10 @@
+import User from "@/models/User";
 import db from "@/utils/db";
+import bcrypt from 'bcrypt'
 import { validateEmail } from "@/utils/validation";
 import { createRouter } from "next-connect"
+import { createActivationToken } from "@/utils/tokens";
+import { sendEmail } from "@/utils/sendEmails";
 const router = createRouter()
 router.post(async (req, res) => {
     try {
@@ -15,7 +19,30 @@ router.post(async (req, res) => {
             return res.status(400).json({ message: "InValid Email" })
         }
 
-        res.status(200).json({ message: "Data recieved successfully" })
+        const user = await User.findOne({ email })
+
+        if (user) {
+            return res.status(400).json({ message: "This user already exists" })
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be atleast 6 characters" })
+        }
+
+        const cryptedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({ name, email, password: cryptedPassword });
+        const addedUser = await newUser.save();
+        const activation_token = createActivationToken({
+            id: addedUser._id.toString(),
+
+        });
+        const url = `${process.env.BASE_URL}/activate/${activation_token}`
+
+        // The mail functionality is not working 
+        // sendEmail(email, url, "", "Summer Collection Offer")
+
+        // res.send(url);
+        // res.status(200).json({ message: "Data recieved successfully" })
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
