c051765681fc3e1fe7dd39194a7fd430a6a58599
Harsh Paliwal <harshbca.21@stu.srmuniversity.ac.in>
2024-03-08 14:13:56 +0530

    Made Category Cards Responsive

--- components/category/index.js
+++ components/category/index.js
@@ -1,8 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import { BsArrowRightCircle } from 'react-icons/bs'
 import styles from './styles.module.scss'
+import { useMediaQuery } from 'react-responsive'
+
 
 export default function Category({ header, products, background }) {
+    const isMedium = useMediaQuery({ query: "(max-width:1300px)" })
+    const isMobile = useMediaQuery({ query: "(max-width:600px)" })
     return (
         <div className={styles.category} style={{ background: `${background}` }}>
             <div className={styles.category__header}>
@@ -10,7 +14,7 @@ export default function Category({ header, products, background }) {
                 <BsArrowRightCircle />
             </div>
             <div className={styles.category__products}>
-                {products.map((product, i) => (
+                {products.slice(0, isMobile ? 6 : isMedium ? 4 : 6).map((product, i) => (
                     <div className={styles.product} key={i}>
 
                         <img src={product.image} alt="" />
--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,15 @@ import Main from "@/components/home/main";
 import FlashDeals from "@/components/home/flashDeals";
 import Category from "@/components/category";
 import { women_accessories, women_dresses, women_shoes } from "@/data/home";
+import { useMediaQuery } from "react-responsive";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ country }) {
   const { data: session } = useSession();
-  console.log(session);
+  const isMedium = useMediaQuery({ query: "(max-width: 850px)" })
+  const isMobile = useMediaQuery({ query: "(max-width: 600px)" })
+  // console.log(session);
   return (
     <>
       <Header country={country} />
@@ -26,9 +29,19 @@ export default function Home({ country }) {
             <Category
               header='Dresses'
               products={women_dresses} background='#5a31f4' />
-            <Category
-              header='Shoes / High Heels'
-              products={women_shoes} background='#3c811f' />
+            {
+              !isMedium &&
+              <Category
+                header='Shoes / High Heels'
+                products={women_shoes} background='#3c811f' />
+            }
+            {
+              isMobile &&
+              <Category
+                header='Shoes / High Heels'
+                products={women_shoes} background='#3c811f' />
+            }
+
             <Category
               header='Accessories'
               products={women_accessories} background='#000' />
