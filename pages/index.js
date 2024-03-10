c78efb162974ff17be08c4974b22ec05d8c59c08
Harsh Paliwal <harshbca.21@stu.srmuniversity.ac.in>
2024-03-10 17:36:33 +0530

    Product Card Component is complete in lectures but it is not working properly till this point

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-import Image from "next/image";
+// Package Imports
+import { useMediaQuery } from "react-responsive";
+// NextJs Imports
 import { Inter } from "next/font/google";
+
+// -- ** --
+// Local Imports 
 import styles from "@/styles/Home.module.scss";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
@@ -8,13 +13,23 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import Main from "@/components/home/main";
 import FlashDeals from "@/components/home/flashDeals";
 import Category from "@/components/category";
+
+// data folder imports
 import { gamingSwiper, homeImprovSwiper, women_accessories, women_dresses, women_shoes, women_swiper } from "@/data/home";
-import { useMediaQuery } from "react-responsive";
+
 import ProductsSwiper from "@/components/productsSwiper";
+
+// Database Imports
+import db from "@/utils/db";
+import Product from "../models/Product";
+import ProductCard from "@/components/productCard";
+
+// -- ** --
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ country }) {
+export default function Home({ country, products }) {
+  // console.log(products)
   const { data: session } = useSession();
   const isMedium = useMediaQuery({ query: "(max-width: 850px)" })
   const isMobile = useMediaQuery({ query: "(max-width: 600px)" })
@@ -58,6 +73,11 @@ export default function Home({ country }) {
             products={homeImprovSwiper}
             bg='5a31f4'
           />
+          <div className={styles.products}>
+            {products.map((product) => (
+              <ProductCard product={product} key={product._id} />
+            ))}
+          </div>
         </div>
       </div>
       <Footer country={country} />
@@ -66,6 +86,7 @@ export default function Home({ country }) {
 }
 
 export async function getServerSideProps() {
+  // This was used to fetch data to get country name , time , flag , weather and other stuff
   // const data = await axios.get('https://api.ipregistry.co/?key=xycrvtfufugeje3j'
   // ).then((res) => {
   //   return res.data.location.country;
@@ -73,8 +94,15 @@ export async function getServerSideProps() {
   //   console.log(err)
   // });
 
+
+  // This will create a connection to mongodb Database 
+  db.connectDb();
+  let products = await Product.find().sort({ createdAt: -1 }).lean();
+
+
   return {
     props: {
+      products: JSON.parse(JSON.stringify(products)),
       // country: {name : data.name , flag: data.flag.emojitwo},
       country: { name: "India", flag: "https://flagcdn.com/in.svg" },
     },
